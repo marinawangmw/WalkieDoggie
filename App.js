b5b8c8b42c7433a528048404995f7a4082ebbb48
Marina Wang <marinawang@Marinas-MacBook-Pro.local>
2021-08-08 16:58:22 -0300

    Add react navigation

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { AuthenticationScreen } from './app/screens';
+import { AuthenticationScreen, OnboardingDetaildScreen } from './app/screens';
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AuthenticationScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Home'
+            component={AuthenticationScreen}
+            options={{ title: 'Authentication' }}
+          />
+          <Stack.Screen
+            name='Onboarding details'
+            component={OnboardingDetaildScreen}
+          />
+        </Stack.Navigator>
+        <StatusBar style='auto' />
+      </View>
+    </NavigationContainer>
   );
 }
 
---
+++ app/screens/OnboardingDetailsScreen/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OnboardingDetailsScreen = () => {
+    return (
+        <View>
+            <Text>Hello OnboardingDetailsScreen!</Text>
+        </View>
+    );
+};
+
+export default OnboardingDetailsScreen;
