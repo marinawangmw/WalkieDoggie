fb4e0b126c87899250ab5be5d00c6c603da44ffd
Uri Lukacher <ulukacher@auntap.com>
2021-08-23 23:20:19 -0300

    advances in preview file before AWS-Upload; advances in retrieve file from bucket once upload

--- app/components/FileUploader/index.jsx
+++ app/components/FileUploader/index.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import { Button, Image, View, Platform, StyleSheet } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system';
 import { uploadFileAws } from '../../utils/aws';
-import { getArrayBufferContent, getMimeType } from '../../utils/files';
+import { getArrayBufferContent, getFileFromUri, getMimeType } from '../../utils/files';
 import { v4 as uuidv4 } from 'uuid';
 
 const FilePicker = () => {
   const [file, setFile] = useState(null);
   const [fileData, setFileData] = useState(null);
+  const [fileRetrieved, setFileRetrieved] = useState(null);
 
   const pickDocument = async () => {
     const result = await DocumentPicker.getDocumentAsync({
@@ -24,7 +25,11 @@ const FilePicker = () => {
         to: uri,
       });
 
+      console.log(result);
+
       setFile(result);
+
+      console.log('Archivo a subir desde', uri);
 
       setFileData({
         uri,
@@ -38,8 +43,8 @@ const FilePicker = () => {
   const uploadFile = () => {
     uploadFileAws(fileData)
       .then((response) => {
-        console.log(response);
-        alert('Archivo subido al bucket!')
+        console.log('Archivo subido en: ', response);
+        setFileRetrieved(response);
       })
       .catch((e) => console.error(e));
   };
@@ -47,7 +52,25 @@ const FilePicker = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick a document..." onPress={pickDocument} />
-      {file && <Button title="Upload to AWS" onPress={uploadFile} />}
+      {fileData && (
+        <View>
+          <Image
+            source={{
+              uri: fileData.uri,
+            }}
+            style={{ width: 50, height: 50 }}
+          />
+          <Button title="Upload to AWS" onPress={uploadFile} />
+        </View>
+      )}
+      {fileRetrieved && (
+        <Image
+          source={{
+            uri: fileRetrieved,
+          }}
+          style={{ width: 100, height: 100 }}
+        />
+      )}
     </View>
   );
 };
--- app/utils/files.js
+++ app/utils/files.js
@@ -1,13 +1,22 @@
 import * as mime from 'react-native-mime-types';
 import { decode } from 'base64-arraybuffer';
 import * as FileSystem from 'expo-file-system';
+import { v4 as uuidv4 } from 'uuid';
 
 export const getMimeType = (uri) => mime.lookup(uri);
 
-export const getArrayBufferContent = async(uri) => {
+export const getArrayBufferContent = async (uri) => {
   const base64 = await FileSystem.readAsStringAsync(uri, {
     encoding: FileSystem.EncodingType.Base64,
   });
 
   return decode(base64);
+};
+
+export const getFileFromUri = async (uri) => {
+  const arr = uri.split('/');
+  const key = arr[arr.length - 1];
+  const baseUri = FileSystem.documentDirectory + key;
+  const { uri: fileUri } = await FileSystem.downloadAsync(uri, baseUri);
+  return fileUri;
 };
