3e8191baf8b5b9460e2932d25998c4309964fa0b
Uri Lukacher <ulukacher@auntap.com>
2021-09-18 19:20:54 -0300

    Rendering MapView; advances in change location listeners (both for walker and owner)

--- app/components/LocationOwnerSide/index.jsx
+++ app/components/LocationOwnerSide/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, View, Platform, Dimensions, SafeAreaView } from 'react-native';
 import MapView, { Marker, AnimatedRegion, MarkerAnimated } from 'react-native-maps';
 import PubNub from 'pubnub';
-import { PubNubProvider, usePubNub } from 'pubnub-react';
 import * as Location from 'expo-location';
 
 // eslint-disable-next-line import/no-unresolved
@@ -21,10 +20,11 @@ const pubnub = new PubNub({
   subscribeKey: PUBNUB_SUBSCRIBE_KEY,
 });
 
-export default class LocationComponent extends React.Component {
+const CHANNEL = 'location_walker';
+
+export default class LocationOwnerSideComponent extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       latitude: LATITUDE,
       longitude: LONGITUDE,
@@ -35,71 +35,32 @@ export default class LocationComponent extends React.Component {
         longitudeDelta: 0,
       }),
     };
-
-    // this.pubnub = new PubNubReact({
-    //   publishKey: 'pub-c-02ef8883-45bc-4b6a-87ca-8732a384e76e',
-    //   subscribeKey: 'sub-c-e9bdffd2-15d2-11ec-9d3c-1ae560ca2970',
-    // });
-    // this.pubnub.init(this);
   }
 
   componentDidMount() {
-    this.watchLocation();
+    this.subscribeToPubNub();
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.latitude !== prevState.latitude) {
-      const message = {
-        latitude: this.state.latitude,
-        longitude: this.state.longitude,
-      };
-      console.log('Proceed to write Pubnub message', new Date(), message);
+  subscribeToPubNub = () => {
+    pubnub.subscribe({
+      channels: [CHANNEL],
+      withPresence: true,
+    });
 
-      pubnub.publish({
-        message,
-        channel: 'location',
-      });
-    }
-  }
+    let self = this;
 
-  componentWillUnmount() {
-    console.log('Proceed to remove subscription');
-    this.suscription.remove();
-  }
+    pubnub.addListener({
+      message: function (obj) {
+        const { latitude, longitude } = obj.message;
+        const newCoordinate = { latitude, longitude };
+        self.state.coordinate = newCoordinate;
 
-  watchLocation = async () => {
-    // const { coordinate } = this.state;
-    this.suscription = await Location.watchPositionAsync(
-      {
-        distanceInterval: 5,
-        accuracy: Location.Accuracy.Highest,
-        timeInterval: 3000,
-      },
-      (position) => {
-        const { latitude, longitude } = position.coords;
-
-        const newCoordinate = {
-          latitude,
-          longitude,
-        };
-
-        // if (Platform.OS === 'android') {
-        //   if (this.marker) {
-        //     console.log('change marker to', newCoordinate);
-        //     this.marker.animateMarkerToCoordinate(newCoordinate, 500); // 500 is the duration to animate the marker
-        //   }
-        // } else {
-        //   coordinate.timing(newCoordinate).start();
-        // }
-
-        this.state.coordinate = newCoordinate;
-
-        this.setState({
+        self.setState({
           latitude,
           longitude,
         });
       },
-    );
+    });
   };
 
   getMapRegion = () => ({
--- app/screens/ChatScreen/index.jsx
+++ app/screens/ChatScreen/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import FileOpener from '../../components/FileOpener';
+import LocationOwnerSideComponent from '../../components/LocationOwnerSide';
 import styles from './styles';
 
 const ChatScreen = () => {
   return (
     <View style={styles.container}>
       <Text>This is Chat!</Text>
-      <FileOpener />
+      <LocationOwnerSideComponent />
     </View>
   );
 };
