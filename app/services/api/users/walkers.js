e756f1a1b1f760fb6c462d3293eb44230afb0ec8
Uri Lukacher <ulukacher@auntap.com>
2021-09-26 16:16:26 -0300

    Add filters for getWalkers service

--- app/services/api/users/walkers.js
+++ app/services/api/users/walkers.js
@@ -1,9 +1,14 @@
 import { HTTP_METHOD, privateRequest } from '../axiosConfig';
 
-export const getWalkers = () => {
+export const getWalkers = ({ score, complete_name, pet_walks_amount }) => {
   const config = {
     method: HTTP_METHOD.GET,
     url: 'users/walkers',
+    params: {
+      score,
+      complete_name,
+      pet_walks_amount,
+    },
   };
   return privateRequest(config)
     .then((data) => ({ result: true, data }))
