71cd249e906915ec079052e4f606cd6f5488c2d3
Uri Lukacher <ulukacher@auntap.com>
2021-09-26 16:22:02 -0300

    Fix(getWalkers): empty object as default

--- app/services/api/users/walkers.js
+++ app/services/api/users/walkers.js
@@ -1,6 +1,6 @@
 import { HTTP_METHOD, privateRequest } from '../axiosConfig';
 
-export const getWalkers = (filters) => {
+export const getWalkers = (filters = {}) => {
   const { score, complete_name, pet_walks_amount } = filters;
   const config = {
     method: HTTP_METHOD.GET,
